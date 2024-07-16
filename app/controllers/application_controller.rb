046d2ee193ca88bc378314760ee50eb2dfafd9e6
tetsupon <te.ka.20230305@gmail.com>
2024-07-16 18:23:08 +0900

    ユーザー情報を保存できる実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, :gender])
+  end
+  
 end
