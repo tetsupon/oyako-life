8de428f765b264bb696731f66b995dc449e82de1
tetsupon <te.ka.20230305@gmail.com>
2024-07-16 19:26:36 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,25 +12,24 @@ class User < ApplicationRecord
     validates :nickname
 
     # 名前のバリデーション
-    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字で入力してください" }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字で入力してください" }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角文字で入力してください" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角文字で入力してください" }
 
     # 名前（カナ）のバリデーション
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください" }
 
     # 性別のバリデーション
-    validates :gender, inclusion: { in: ['male', 'female'], message: "は男性か女性を選択してください" }
+    validates :gender, inclusion: { in: ['male', 'female'], message: "男性か女性を選択してください" }
 
     # 生年月日のバリデーション
     validates :birth_day
 
     # Emailのバリデーション
-    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "は有効なメールアドレスを入力してください" }
+    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "有効なメールアドレスを入力してください" }
   end
 
   # パスワードのバリデーション
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }, confirmation: true
-  validates :password_confirmationdd_confirmation
+  validates_format_of :password, with: PASSWORD_REGEX, message: "有効なメールアドレスを入力してください"
 end  
