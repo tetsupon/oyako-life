ab1da6c63df30f5f1408529ca355853a1873c30d
tetsupon <te.ka.20230305@gmail.com>
2024-07-21 10:35:45 +0900

    エラーメッセージの順番の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,20 +14,20 @@ class User < ApplicationRecord
     # ニックネームのバリデーション
     validates :nickname
 
+    # 名前のバリデーション
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  
+
+    # 名前（カナ）のバリデーション
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }    
+
     # 性別のバリデーション
     validates :gender, inclusion: { in: ['male', 'female'] }
 
     # 生年月日のバリデーション
     validates :birth_day
-
-    # 名前のバリデーション
-    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  
     
-    # 名前（カナ）のバリデーション
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-
   end
     
 
