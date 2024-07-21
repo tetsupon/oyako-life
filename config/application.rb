3b72af04f73441d02d3cdcb2a649199b92641021
tetsupon <te.ka.20230305@gmail.com>
2024-07-21 17:07:23 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module BoardApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    #　以下の記述を追記する(設定必須)
+    # 以下の記述を追記する(設定必須)
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
 
