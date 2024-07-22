c539efc83a8d23f52482620d1f948c5b5f81b4a6
tetsupon <te.ka.20230305@gmail.com>
2024-07-22 18:50:51 +0900

    ユーザー編集ページの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'ユーザー情報が更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :current_password)
   end
 
 end
