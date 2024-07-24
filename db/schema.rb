c0fd07d90a2b0df03f3237829f3a26020c0409f7
tetsupon <te.ka.20230305@gmail.com>
2024-07-24 21:40:41 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_14_022709) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_24_104832) do
+  create_table "children", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "gender", null: false
+    t.date "birth_day", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_children_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -30,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_14_022709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "children", "users"
 end
