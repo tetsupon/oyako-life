214ed7ea26e38b283b90e2702001b060f171760d
tetsupon <te.ka.20230305@gmail.com>
2024-07-25 19:21:09 +0900

    バリデーションの設定

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,23 @@
 class Child < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    # ニックネームのバリデーション
+    validates :nickname
+
+    # 名前のバリデーション
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  
+
+    # 名前（カナ）のバリデーション
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }    
+
+    # 性別のバリデーション
+    validates :gender, inclusion: { in: ['male', 'female'] }
+
+    # 生年月日のバリデーション
+    validates :birth_day
+    
+  end
 end
