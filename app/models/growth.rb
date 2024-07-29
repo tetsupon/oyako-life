5d3f625f797828efa91f6140c215118cae45373d
tetsupon <te.ka.20230305@gmail.com>
2024-07-29 20:34:09 +0900

    モデル記述

--- app/models/growth.rb
+++ app/models/growth.rb
@@ -1,2 +1,10 @@
 class Growth < ApplicationRecord
+  belongs_to :child
+
+  with_options presence: true do
+    validates :record_date
+    validates :growth_type_id
+    validates :height, numericality: { greater_than: 0 }
+    validates :weight, numericality: { greater_than: 0 }
+  end
 end
