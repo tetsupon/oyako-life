03cbc542f756936384e17a1a372919f5f855ca9b
tetsupon <te.ka.20230305@gmail.com>
2024-07-29 20:36:43 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :children
+  resources :children do
+    resources :growths, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
   root to: "home#index"
   resources :users, only: [:edit, :update]
 end
