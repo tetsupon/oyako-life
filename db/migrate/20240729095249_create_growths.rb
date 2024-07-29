a1db22a0f36715f765aa04a1e79fb743487db973
tetsupon <te.ka.20230305@gmail.com>
2024-07-29 18:53:27 +0900

    growthモデル作成

---
+++ db/migrate/20240729095249_create_growths.rb
@@ -0,0 +1,8 @@
+class CreateGrowths < ActiveRecord::Migration[7.0]
+  def change
+    create_table :growths do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/growths.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :growth do
+    
+  end
+end
