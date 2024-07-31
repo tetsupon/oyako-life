3144c25936d04f3ec998ced6b5df447657790a06
tetsupon <te.ka.20230305@gmail.com>
2024-07-31 21:32:31 +0900

    成長記録一覧ページのコントローラー設定

--- app/controllers/growths_controller.rb
+++ app/controllers/growths_controller.rb
@@ -1,4 +1,12 @@
 class GrowthsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_child
+
   def index
+    @growths = @child.growths.order(record_date: :desc)
+  end
+
+  def set_child
+    @child = current_user.children.find(params[:child_id])
   end
 end
