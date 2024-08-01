89e08f0bb22e4031f9a9affa2a03a740cb27d687
tetsupon <te.ka.20230305@gmail.com>
2024-08-01 19:31:55 +0900

    新規成長記録のコントローラー作成

--- app/controllers/growths_controller.rb
+++ app/controllers/growths_controller.rb
@@ -6,7 +6,26 @@ class GrowthsController < ApplicationController
     @growths = @child.growths.order(record_date: :desc)
   end
 
+  def new
+    @growth = @child.growths.new
+  end
+
+  def create
+    @growth = @child.growths.new(growth_params)
+    if @growth.save
+      redirect_to child_growths_path(@child)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
   def set_child
     @child = current_user.children.find(params[:child_id])
+  end
+
+  def growth_params
+    params.require(:growth).permit(:record_date, :growth_type_id, :height, :weight, :description)
   end
 end
