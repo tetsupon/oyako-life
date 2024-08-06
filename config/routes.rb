835bfdebab4a8304836de9f671c1772f400d24bc
tetsupon <te.ka.20230305@gmail.com>
2024-08-06 13:33:17 +0900

    成長記録一覧ページから詳細ページへの遷移できるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'growths/index'
   devise_for :users
   resources :children do
-    resources :growths, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :growths, only: [:index, :new, :create, :edit, :show, :update, :destroy]
   end
   root to: "home#index"
   resources :users, only: [:edit, :update]
