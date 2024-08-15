19e8194fde9442a57b404eefb3792906793a4236
tetsupon <te.ka.20230305@gmail.com>
2024-08-15 18:37:24 +0900

    成長記録のfactoriesの設定

--- spec/factories/growths.rb
+++ spec/factories/growths.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :growth do
+    association :child
+    
+    record_date { Date.today }
+    growth_type_id { 1 }
+    height { 50.5 }
+    weight { 3.5 }
     
   end
 end
