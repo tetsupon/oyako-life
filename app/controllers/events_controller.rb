c5847e78413b6aeda28fda5c20873fd560eb5345
tetsupon <te.ka.20230305@gmail.com>
2024-08-17 10:07:56 +0900

    コントローラーに記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,14 @@
 class EventsController < ApplicationController
+  before_action :set_child
+
+  def index
+    @events = @child.events
+  end
+
+  private
+
+  def set_child
+    @child = Child.find(params[:child_id])
+  end
+
 end
