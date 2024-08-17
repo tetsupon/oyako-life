f3b7621fff6148b58fd4c9819358c808e350fcb0
tetsupon <te.ka.20230305@gmail.com>
2024-08-17 10:15:43 +0900

    バリデーションの設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :child
+
+  with_options presence: true do
+    validates :record_date
+    validates :event_type_id
+    validates :description
+  end
 end
