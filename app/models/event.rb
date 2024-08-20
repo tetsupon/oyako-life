e9e94d141be1eca8779fc88909630f78cbd7156b
tetsupon <te.ka.20230305@gmail.com>
2024-08-20 18:31:55 +0900

    詳細フォームの記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :child
-  has_one_attached :image
+  has_many_attached :images  # 複数の画像を添付する場合
   
   belongs_to_active_hash :event_type
 
