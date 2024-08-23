e487c707cca0f61a5e95d1f620c6802a3fc0f348
tetsupon <te.ka.20230305@gmail.com>
2024-08-23 19:17:19 +0900

    予防接種コントローラに@childセット

--- app/controllers/vaccinations_controller.rb
+++ app/controllers/vaccinations_controller.rb
@@ -1,5 +1,13 @@
 class VaccinationsController < ApplicationController
+  before_action :set_child
+
   def index
     @vaccinations = @child.vaccinations.order(scheduled_date: :asc)
+  end
+
+  private
+
+  def set_child
+    @child = Child.find(params[:child_id])
   end
 end
