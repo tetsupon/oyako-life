c4a7f871b9cadb0b729d7f366670ee44e3a4a7b5
tetsupon <te.ka.20230305@gmail.com>
2024-08-24 16:33:31 +0900

    予防接種情報を保存機能の実装

--- app/controllers/vaccinations_controller.rb
+++ app/controllers/vaccinations_controller.rb
@@ -9,10 +9,23 @@ class VaccinationsController < ApplicationController
     @vaccination = @child.vaccinations.new
   end
 
+  def create
+    @vaccination = @child.vaccinations.new(vaccination_params)
+    if @vaccination.save
+      redirect_to child_vaccinations_path(@child)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
   def set_child
     @child = Child.find(params[:child_id])
+  end
+
+  def vaccination_params
+    params.require(:vaccination).permit(:vaccination_name, :scheduled_date, :administered_date, :administered_by, :description)
   end
 end
