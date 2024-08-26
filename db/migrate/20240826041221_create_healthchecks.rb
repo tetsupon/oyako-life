513bede96f6282090e2843ff915fcdf709e7a41a
tetsupon <te.ka.20230305@gmail.com>
2024-08-26 13:15:57 +0900

    健康診断記録のマイグレーションファイル記述・実行

--- db/migrate/20240826041221_create_healthchecks.rb
+++ db/migrate/20240826041221_create_healthchecks.rb
@@ -1,7 +1,12 @@
 class CreateHealthchecks < ActiveRecord::Migration[7.0]
   def change
     create_table :healthchecks do |t|
-
+      t.references :child, null: false, foreign_key: true
+      t.string :checkup_name, null: false
+      t.datetime :scheduled_date, null: false
+      t.date :administered_date, null: false
+      t.string :doctor_name
+      t.text :description
       t.timestamps
     end
   end
