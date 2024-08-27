52fe7b43b5957b074c290d75b42352e277bec187
tetsupon <te.ka.20230305@gmail.com>
2024-08-27 19:38:37 +0900

    健康診断一覧のフォーム修正と追加情報を保存できるように

--- app/controllers/healthchecks_controller.rb
+++ app/controllers/healthchecks_controller.rb
@@ -9,9 +9,22 @@ class HealthchecksController < ApplicationController
     @healthcheck = @child.healthchecks.new
   end
 
+  def create
+    @healthcheck = @child.healthchecks.new(healthcheck_params)
+    if @healthcheck.save
+      redirect_to child_healthchecks_path(@child)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_child
     @child = Child.find(params[:child_id])
+  end
+
+  def healthcheck_params
+    params.require(:healthcheck).permit(:checkup_name, :scheduled_date, :administered_date, :doctor_name, :description)
   end
 end
