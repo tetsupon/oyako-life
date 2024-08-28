e3f6fc5d1f7cf2dee72823d1eb03973b3d1d039f
tetsupon <te.ka.20230305@gmail.com>
2024-08-28 19:32:10 +0900

    エラーメッセージの表示を実装

--- app/models/healthcheck.rb
+++ app/models/healthcheck.rb
@@ -1,4 +1,13 @@
 class Healthcheck < ApplicationRecord
   belongs_to :child
-  
+
+  with_options presence: true do
+    validates :checkup_name
+    validates :scheduled_date
+    validates :doctor_name
+  end
+ 
+  # 詳細はオプションだが、最大文字数を制限
+  validates :description, length: { maximum: 500 }, allow_blank: true
+
 end
