878c8a41ed358cd8ade6f7bc32fac6046f6bb4c5
tetsupon <te.ka.20230305@gmail.com>
2024-08-30 08:54:16 +0900

    食事管理のバリデーション実装

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,4 +3,11 @@ class Meal < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :meal_type
+
+
+  with_options presence: true do
+    validates :meal_date
+    validates :meal_type_id
+    validates :meal_name
+  end
 end
