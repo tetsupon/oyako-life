964342483f1b332c3a9d391dcf230c37f2a92f4d
tetsupon <te.ka.20230305@gmail.com>
2024-08-30 08:36:59 +0900

    食事追加機能の実装

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -9,9 +9,22 @@ class MealsController < ApplicationController
     @meal = @child.meals.new
   end
 
+  def create
+    @meal = @child.meals.build(meal_params)
+    if @meal.save
+      redirect_to child_meals_path(@child)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_child
     @child = Child.find(params[:child_id])
   end
+
+  def meal_params
+    params.require(:meal).permit(:meal_date, :meal_type_id, :meal_name, :quantity, :description)
+  end  
 end
