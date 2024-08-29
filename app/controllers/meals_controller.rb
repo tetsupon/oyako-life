471b6b081ee248ee9f14e3c2f8aca9dfa2a615b8
tetsupon <te.ka.20230305@gmail.com>
2024-08-29 15:20:53 +0900

    食事管理一覧ページのコントーラー設定

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,13 @@
 class MealsController < ApplicationController
+  before_action :set_child
+
+  def index
+    @meals = @child.meals.order(meal_date: :desc)
+  end
+
+  private
+
+  def set_child
+    @child = Child.find(params[:child_id])
+  end
 end
