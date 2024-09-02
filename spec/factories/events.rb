2ef445b2e280661f5a6d3cf6dd979501786d2421
tetsupon <te.ka.20230305@gmail.com>
2024-09-03 06:53:02 +0900

    出来事記録のFactoryBot実装

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :event do
-    
+    record_date { Faker::Date.backward(days: 14) }
+    event_type_id { 1 } # ActiveHashで定義されている有効なIDを指定
+    description { "Sample event description" }
+    association :child
+
   end
 end
