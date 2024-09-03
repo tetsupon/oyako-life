5f09c299fb0d0dab7e0ac22cfed173df42c2cc33
tetsupon <te.ka.20230305@gmail.com>
2024-09-04 07:27:38 +0900

    予防接種FactoryBotの修正

--- spec/factories/vaccinations.rb
+++ spec/factories/vaccinations.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     vaccination_name { 'BCG' }
     scheduled_date   { Faker::Date.forward(days: 23) }
     administered_date { Faker::Date.backward(days: 2) }
+
+    association :child  # これにより、Childモデルと関連付け
   end
 end
