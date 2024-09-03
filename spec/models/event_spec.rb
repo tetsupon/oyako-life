743e0d4200f129a6e93490117cf4ec8e7be6a7c3
tetsupon <te.ka.20230305@gmail.com>
2024-09-03 18:13:52 +0900

    出来事記録の単体テストコードの実装

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = FactoryBot.build(:event)
+  end
+
+  describe '出来事記録の保存' do
+    context '出来事記録が保存できる場合' do
+      it '全ての項目が正しく入力されていれば保存できる' do
+        expect(@event).to be_valid
+      end
+
+      it '画像がなくても保存できる' do
+        @event.image = nil
+        expect(@event).to be_valid
+      end
+    end
+
+    context '出来事記録が保存できない場合' do
+      it '日付が空だと保存できない' do
+        @event.record_date = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include('出来事の日付を入力してください')
+      end
+
+      it '出来事の種類が空だと保存できない' do
+        @event.event_type_id = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include('出来事の種類を入力してください')
+      end
+
+      it '詳細説明が空だと保存できない' do
+        @event.description = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include('詳細説明を入力してください')
+      end
+    end
+  end
 end
