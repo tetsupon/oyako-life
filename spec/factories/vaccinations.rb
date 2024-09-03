73baa537455ea1cabda4c39a132eacc112f6e051
tetsupon <te.ka.20230305@gmail.com>
2024-09-04 07:21:10 +0900

    予防接種のFactoryBotの実装

--- spec/factories/vaccinations.rb
+++ spec/factories/vaccinations.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :vaccination do
-    
+    vaccination_name { 'BCG' }
+    scheduled_date   { Faker::Date.forward(days: 23) }
+    administered_date { Faker::Date.backward(days: 2) }
   end
 end
