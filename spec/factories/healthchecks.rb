c8e4f5cd2d6d1c647969c421f713d4a182ee7ae8
tetsupon <te.ka.20230305@gmail.com>
2024-09-05 06:19:11 +0900

    健康診断記録機能のFactoryBot実装

--- spec/factories/healthchecks.rb
+++ spec/factories/healthchecks.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
-  factory :healthcheck do
-    
+  FactoryBot.define do
+    factory :healthcheck do
+      checkup_name { "定期健康診断" }
+      scheduled_date { Date.today }
+      doctor_name { "山田 太郎" }
+      description { "特に異常はありませんでした。" }
+      association :child
+    end
   end
+  
 end
