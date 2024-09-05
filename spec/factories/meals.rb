e6e7041ee08f9efe95f3a12822d4ef540f899ad2
tetsupon <te.ka.20230305@gmail.com>
2024-09-06 06:42:54 +0900

    食事管理機能のFactoryBot実装

--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :meal do
-    
+    meal_date { Faker::Date.backward(days: 14) }
+    meal_type_id { 1 }
+    meal_name { "おかゆ" }
+    association :child
   end
 end
+
