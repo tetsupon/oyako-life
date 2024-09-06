15048f07cb8e92a43ea67fc32e626f90644f6bd1
tetsupon <te.ka.20230305@gmail.com>
2024-09-07 06:53:54 +0900

    バリデーションの追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ApplicationRecord
 
   with_options presence: true do
     validates :record_date
-    validates :event_type_id
+    validates :event_type_id, numericality: { other_than: 0, message: 'は0以外の値にしてください' }
     validates :description
   end
 end
--- app/models/growth.rb
+++ app/models/growth.rb
@@ -7,7 +7,7 @@ class Growth < ApplicationRecord
 
   with_options presence: true do
     validates :record_date
-    validates :growth_type_id
+    validates :growth_type_id, numericality: { other_than: 0, message: 'は0以外の値にしてください' }
     validates :height, numericality: { greater_than: 0 }
     validates :weight, numericality: { greater_than: 0 }
   end
